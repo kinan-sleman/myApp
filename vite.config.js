497d0f9e8a92ffa3e94275a6bbba3e981d00dade
Kinan Sleman <kksleman50@gmail.com>
2024-07-27 13:46:48 +0300

    vuejs-search-meals

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // Determine base URL based on environment
 const isProduction = process.env.NODE_ENV === 'production';
-const base = isProduction ? '/nexcent/' : '/';
+const base = isProduction ? '/vuejs-search-meals/' : '/';
 
 // https://vitejs.dev/config/
 export default defineConfig({
